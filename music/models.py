ca0c6bec0f8786312e3a68f6e638f45fe38d341b
Lucy Ruiz <lucyruiz846@gmail.com>
2022-10-21 15:20:20 -0700

    created song table with required properties

---
+++ music/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.forms import CharField, DateField
+
+# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=225)
+    artist = models.CharField(max_length=225)
+    album = models.CharField(max_length=225)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=225)
